2cfa5cde11eb66715e76375729a19d3faf815658
Charlie Root <root@freebsd-002.vps1.sfs.it>
2018-11-06 10:15:18 +0100

    add mysql remote server

--- subs/create-db.sh
+++ subs/create-db.sh
@@ -17,6 +17,7 @@ test -s "/root/SFSit_MST.conf.sh" && SETTINGS_FILE="/root/SFSit_MST.conf.sh"
 if [ -s "$SETTINGS_FILE" ]; then
 	VHOSTS_DIR="$(sh "$SETTINGS_FILE" VHOSTS_DIR)"
 	MYSQL_ROOT_PWD="$(sh "$SETTINGS_FILE" MYSQL_ROOT_PWD)"
+	MYSQL_SERVER="$(sh "$SETTINGS_FILE" MYSQL_SERVER)"
 fi
 PWD_SRC="$(pwd)"
 cd $(dirname $0) 
@@ -33,10 +34,15 @@ VHOST_ACCOUNTFILE="$VHOSTS_DIR/$VHOST/account.txt";
 [ -s "$VHOST_ACCOUNTFILE" ] || exit_with_error "ERROR: CANNOT LOAD 'account.txt' FOR $VHOST"
 USER="$(cat $VHOST_ACCOUNTFILE | grep 'USER:' | sed 's/^USER:\s*//' | sed 's/^[[:blank:]]*//g' | sed 's/^[[:blank:]]*//g')"
 PWD_MYSQL="$(cat $VHOST_ACCOUNTFILE | grep 'PWD_MYSQL:' | sed 's/^PWD_MYSQL:\s*//')"
+if [ "x" != "x$MYSQL_SERVER" ]; then
+	remote="-h $MYSQL_SERVER"
+else
+	remote=''
+fi
 ( cat "../templates/create-db-user.sql.tpl" \
 	| sed -E "s/\\{\\\$USER\\}/$USER/g" \
 	| sed -E "s/\\{\\\$PWD_MYSQL\\}/$(echo $PWD_MYSQL | sed 's;&;\\&;g')/g" \
-	| mysql --password="$MYSQL_ROOT_PWD" ) || exit_with_error "ERROR: CREATING USER AND DB"
+	| mysql $remote --password="$MYSQL_ROOT_PWD" ) || exit_with_error "ERROR: CREATING USER AND DB"
 echo "DB user:'$USER' and db:'$USER' FOR '$VHOST' CREATED" 
 cd "$PWD_SRC"
 exit 0
