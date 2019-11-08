524bc7f24c2c076e8142509cc7422420e0f33094
Charlie Root <root@freebsd-devel-003.lan.sfs.it>
2019-11-08 08:30:48 +0000

    Changes to be committed: 	modified:   fix_joomla_mod.sh

--- fix_joomla_mod.sh
+++ fix_joomla_mod.sh
@@ -53,11 +53,12 @@ fi
 cd "$VHOSTS_DIR/$VHOST/$HTTPDOCS_DIR"
 USE_FTP="$(cat configuration.php | grep ftp_enable | grep -v -E -e '\s*\/\/' | sed -E -e "s/([^']*')([^'])('.*)/\2/")"
 if [ "x$USER" = "xDEVEL" ]; then
+	echo 'FIX ALL FILES TO $WWW_USER:$WWW_USER files to 660 / dir to 660'
     chown $CHOWN_OPTIONS -R $WWW_USER .
     find . ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
     find . -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 	if [ "x$USE_FTP" = "x1" ]; then
-		echo 'SET CONFIGURATION FTP ON'
+		echo 'SET CONFIGURATION FTP OFF'
 		cat configuration.php | sed -e "s/ftp_enable = '1';/ftp_enable = '0';/" > "/tmp/${VHOST}-configuration.php"
 		rm configuration.php
 		mv "/tmp/${VHOST}-configuration.php" configuration.php
@@ -66,24 +67,20 @@ if [ "x$USER" = "xDEVEL" ]; then
     exit
 else
 	if [ "x$USE_FTP" = "x0" ]; then
-		echo 'SET CONFIGURATION FTP'
+		echo 'SET CONFIGURATION FTP ON'
 		cat configuration.php | sed -e "s/ftp_enable = '0';/ftp_enable = '1';/" > "/tmp/${VHOST}-configuration.php"
 		rm configuration.php
 		mv "/tmp/${VHOST}-configuration.php" configuration.php
 	fi
 fi
 
-echo 'FIX ALL FILES VHOST MOD'
-find . ! -type f -exec chmod $CHMOD_OPTIONS 750 '{}' \;
-echo 'FIX ALL DIRS VHOST MOD'
-find . -type f -exec chmod $CHMOD_OPTIONS 640 '{}' \;
-echo 'FIX ALL DIRS VHOST OWN'
-find . -exec chown $CHOWN_OPTIONS "$USER":$WWW_USER '{}' \;
+echo 'FIX ALL FILES VHOST MOD files 640 / dir 750'
+find . -exec chown $CHOWN_OPTIONS "$USER":$WWW_USER '{}' -type f  \( -exec chmod $CHMOD_OPTIONS 640 '{}' \) -o -exec chmod $CHMOD_OPTIONS 750 '{}' \;
 if [ -d logs ]; then
 	echo 'FIX logs/'
     chmod $CHMOD_OPTIONS -R 770 logs 
     chown $CHOWN_OPTIONS -R $WWW_USER:$WWW_GROUP logs 
-elif [ ! -d administrator/logs ]; then
+elif [ -e administrator -a ! -d administrator/logs ]; then
 	echo 'CREATE administrator/logs/'
     mkdir administrator/logs
 fi
@@ -92,31 +89,31 @@ if [ -d administrator/logs ]; then
     chmod $CHMOD_OPTIONS -R 770 administrator/logs 
     chown $CHOWN_OPTIONS -R $WWW_USER:$WWW_GROUP administrator/logs 
 fi
+echo 'FIX tmp'
 [ -d tmp ] || mkdir tmp
-	echo 'FIX tmp'
 chmod 770 tmp
 chown -R $CHOWN_OPTIONS -R $WWW_USER:$WWW_GROUP tmp 
-find tmp/* -type d -exec chmod $CHMOD_OPTIONS 770 {} \;
-find tmp/* ! -type d -exec chmod $CHMOD_OPTIONS 660 {} \;
+find tmp/* -type d -exec chmod $CHMOD_OPTIONS 770 {} \; \
+			-o -exec chmod $CHMOD_OPTIONS 660 {} \;
 if [ -d cache ]; then
 	echo 'FIX cache'
     chown -R $CHOWN_OPTIONS -R $WWW_USER:$WWW_GROUP cache
     chmod $CHOWN_OPTIONS 770 cache
-    find cache/* -type d -exec chmod $CHMOD_OPTIONS 770 {} \;
-    find cache/* ! -type d -exec chmod $CHMOD_OPTIONS 660 {} \;
+    find cache/* -type d -exec chmod $CHMOD_OPTIONS 770 {} \; \
+					-o -type d -exec chmod $CHMOD_OPTIONS 660 {} \;
 	echo 'FIX administrator/cache/'
 	chmod 770 administrator/cache
     chown -R $CHOWN_OPTIONS -R $WWW_USER:$WWW_GROUP administrator/cache
-    find administrator/cache/* -type d -exec chmod $CHMOD_OPTIONS 770 {} \; -exec chown $CHOWN_OPTIONS -R  $WWW_USER:$WWW_GROUP {} \;
-    find administrator/cache/* ! -type d -exec chmod $CHMOD_OPTIONS 660 {} \; -exec chown $CHOWN_OPTIONS -R  $WWW_USER:$WWW_GROUP {} \;
+    find administrator/cache/* -type d \( -exec chmod $CHMOD_OPTIONS 770 {} \; -exec chown $CHOWN_OPTIONS -R  $WWW_USER:$WWW_GROUP {} \; \) \
+										-o \( -exec chmod $CHMOD_OPTIONS 660 {} \; -exec chown $CHOWN_OPTIONS -R  $WWW_USER:$WWW_GROUP {} \; \)
 fi
 #installation dir
 if [ -d installation ]; then
 	echo 'FIX installation'
     chmod 770 installation
     chown $CHOWN_OPTIONS -R $WWW_USER:$WWW_GROUP installation
-    find installation/* -type d -exec chmod $CHMOD_OPTIONS 770 {} \;
-    find installation/* ! -type d -exec chmod $CHMOD_OPTIONS 660 {} \;
+    find installation/* -type d -exec chmod $CHMOD_OPTIONS 770 {} \; \
+								-o -exec chmod $CHMOD_OPTIONS 660 {} \;
 
 fi
 
@@ -130,55 +127,55 @@ fi
 if [ -d "images/galleries/" ]; then
 	echo 'FIX images/galleries/'
     chown $CHOWN_OPTIONS -R $WWW_USER images/galleries/
-    find images/galleries/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-    find images/galleries/ -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+    find images/galleries/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; \
+									-o -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 fi
 #joomgallery data directories
 if [ -d "images/joomgallery/" ]; then
 	echo 'FIX images/joomgallery/'
     chown $CHOWN_OPTIONS -R $WWW_USER images/joomgallery/
-    find images/joomgallery/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-    find images/joomgallery/ -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+    find images/joomgallery/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; \
+									-o -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 fi
 #vmart data directories
 if [ -d "images/virtuemart" ]; then
 	echo 'FIX images/virtuemart/'
     chown $CHOWN_OPTIONS -R $WWW_USER images/virtuemart/
-    find images/virtuemart/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-    find images/virtuemart/ -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+    find images/virtuemart/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; \
+									-o -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 fi
 #com_easycreator data directories
 if [ -d "components/com_easycreator" ]; then
 	echo 'FIX components/com_easycreator'
     chown $CHOWN_OPTIONS -R $WWW_USER components/com_easycreator
-    find components/com_easycreator ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-    find components/com_easycreator -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+    find components/com_easycreator ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; -o \
+											-o -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 	if [ -d "administrator/components/com_easycreator/data/" ]; then
 		echo 'FIX administrator/components/com_easycreator/data/'
 		chown $CHOWN_OPTIONS -R $WWW_USER administrator/components/com_easycreator/data/
-		find administrator/components/com_easycreator/data/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-		find administrator/components/com_easycreator/data/ -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+		find administrator/components/com_easycreator/data/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; \
+																	-o -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 	fi
 fi
 if [ -d "media/com_uniterevolution2" ]; then
 	echo 'FIX media/com_uniterevolution2'
     chown $CHOWN_OPTIONS -R $WWW_USER media/com_uniterevolution2/assets/rs-plugin
-    find media/com_uniterevolution2/assets/rs-plugin/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-    find media/com_uniterevolution2/assets/rs-plugin/ -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+    find media/com_uniterevolution2/assets/rs-plugin/ ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; \
+														-o -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 fi
 #component chronoforms 
 if [ -d "libraries/cegcore2/cache" ]; then
 	echo 'FIX libraries/cegcore2/cache'
     chown $CHOWN_OPTIONS -R $WWW_USER libraries/cegcore2/cache
-    find libraries/cegcore2/cache ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-    find libraries/cegcore2/cache -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+    find libraries/cegcore2/cache ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; \
+										-o  -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 fi
 #component widgetkit
 if [ -d "cache/widgetkit" ]; then
 	echo 'FIX cache/widgetkit'
     chown $CHOWN_OPTIONS -R $WWW_USER cache/widgetkit
-    find cache/widgetkit ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \;
-    find cache/widgetkit -type f -exec chmod $CHMOD_OPTIONS 660 '{}' \;
+    find cache/widgetkit ! -type f -exec chmod $CHMOD_OPTIONS 770 '{}' \; \
+								-o -exec chmod $CHMOD_OPTIONS 660 '{}' \;
 fi
 cd "$PWD_SRC"
 exit 0
